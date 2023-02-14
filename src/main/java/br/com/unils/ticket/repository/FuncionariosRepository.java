e52496adf32af8f543a0cc226357f3bff2faaa22
Adeilton <filhoaguiar20@gmail.com>
2023-02-13 21:23:56 -0300

    feat: query que verifica existencia de e-mail

--- src/main/java/br/com/unils/ticket/repository/FuncionariosRepository.java
+++ src/main/java/br/com/unils/ticket/repository/FuncionariosRepository.java
@@ -1,6 +1,8 @@
 package br.com.unils.ticket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.unils.ticket.model.FuncionariosModel;
@@ -8,4 +10,7 @@ import br.com.unils.ticket.model.FuncionariosModel;
 @Repository
 public interface FuncionariosRepository extends JpaRepository<FuncionariosModel, Long> {
 
+	@Query("SELECT f FROM FuncionariosModel f WHERE f.email=:email")
+	FuncionariosModel verifyEmail(@Param(value = "email") String email);
+	
 }
