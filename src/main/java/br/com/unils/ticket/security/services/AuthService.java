bb1929b58c7998377e86323752ea749a157cc240
Adeilton <filhoaguiar20@gmail.com>
2023-02-13 21:26:20 -0300

    feat: metódo de criação de usuário

--- src/main/java/br/com/unils/ticket/security/services/AuthService.java
+++ src/main/java/br/com/unils/ticket/security/services/AuthService.java
@@ -1,5 +1,8 @@
 package br.com.unils.ticket.security.services;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -7,11 +10,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.unils.ticket.model.FuncionariosModel;
 import br.com.unils.ticket.security.Jwt.JwtTokenProvider;
+import br.com.unils.ticket.security.model.PermissionModel;
 import br.com.unils.ticket.security.model.UserModel;
 import br.com.unils.ticket.security.model.vo.LoginCredentialsVO;
 import br.com.unils.ticket.security.model.vo.TokenVO;
+import br.com.unils.ticket.security.model.vo.UserVO;
+import br.com.unils.ticket.security.model.vo.UserVOResponse;
 import br.com.unils.ticket.security.repositories.UserRepository;
+import br.com.unils.ticket.service.FuncionariosService;
 
 @Service
 public class AuthService {
@@ -22,6 +30,8 @@ public class AuthService {
 	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private FuncionariosService funcionariosService;
 
 	public ResponseEntity<TokenVO> signin(LoginCredentialsVO login) {
 		try {
@@ -39,6 +49,29 @@ public class AuthService {
 		} catch (Exception e) {
 			throw new BadCredentialsException("Invalid username/password!");
 		}
+	}
+
+	public UserVOResponse createUser(UserVO obj) {
+		FuncionariosModel funcionario = funcionariosService.verifyEmail(obj.getEmail());
+		if (funcionario != null) {
+			obj.setPassword(passwordEncoder.encode(obj.getPassword()));
+			UserModel user = instanciaUser(obj, funcionario);
+			userRepository.save(user);
+			return new UserVOResponse(user.getUsername(), user.getFullName());
+		}
+		throw new RuntimeException("E-mail inexistente!");
+	}
+
+	private UserModel instanciaUser(UserVO obj, FuncionariosModel funcionario) {
+		UserModel user = new UserModel(obj.getUsername(), obj.getFullName(), obj.getPassword());
+		return setParams(user, funcionario);
+	}
+
+	private UserModel setParams(UserModel user, FuncionariosModel funcionario) {
+		user.setFuncionario(funcionario);
+		List<PermissionModel> permission = Arrays.asList(new PermissionModel(2L, "FUN"));
+		user.setPermissions(permission);
+		return user;
 	}
 
 	public ResponseEntity<TokenVO> refreshToken(String username, String refreshToken) {
